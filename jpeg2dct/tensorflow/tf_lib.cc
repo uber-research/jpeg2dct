0fde66ceedfcc1930e85e1fe377ee234829fc753
Lionel Gueguen <lgueguen@uber.com>
2018-07-02 14:02:35 -0600

    add new license

--- jpeg2dct/tensorflow/tf_lib.cc
+++ jpeg2dct/tensorflow/tf_lib.cc
@@ -1,3 +1,12 @@
+//Copyright (c) 2018 Uber Technologies, Inc.
+//
+//Licensed under the Uber Non-Commercial License (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at the root directory of this project.
+//
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/shape_inference.h"
