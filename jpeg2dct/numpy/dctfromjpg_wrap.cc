11fa6e920c167935ea4d2134fccb1c263e709747
Lionel Gueguen <lgueguen@uber.com>
2018-07-02 14:24:51 -0600

    remove copyright in code generated automatically

--- jpeg2dct/numpy/dctfromjpg_wrap.cc
+++ jpeg2dct/numpy/dctfromjpg_wrap.cc
@@ -9,15 +9,6 @@
  * interface file instead.
  * ----------------------------------------------------------------------------- */
 
-//Copyright (c) 2018 Uber Technologies, Inc.
-//
-//Licensed under the Uber Non-Commercial License (the "License");
-//you may not use this file except in compliance with the License.
-//You may obtain a copy of the License at the root directory of this project.
-//
-//See the License for the specific language governing permissions and
-//limitations under the License.
-
 #define SWIG_PYTHON_STRICT_BYTE_CHAR
 
 
--- jpeg2dct/numpy/dctfromjpg_wrapper.py
+++ jpeg2dct/numpy/dctfromjpg_wrapper.py
@@ -7,16 +7,6 @@
 # the SWIG interface file instead.
 
 
-# Copyright (c) 2018 Uber Technologies, Inc.
-#
-# Licensed under the Uber Non-Commercial License (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at the root directory of this project.
-#
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 
 from sys import version_info as _swig_python_version_info
 if _swig_python_version_info >= (2, 7, 0):
