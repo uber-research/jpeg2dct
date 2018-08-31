cd615381404c823af75d949a05a964f0de36143b
Lionel Gueguen <lgueguen@uber.com>
2018-08-31 17:25:07 -0600

    remove duplicate

--- jpeg2dct/tensorflow/__init__.py
+++ jpeg2dct/tensorflow/__init__.py
@@ -11,8 +11,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import sysconfig
-
 from tensorflow.python.framework import load_library
 from tensorflow.python.framework import ops
 from tensorflow.python.platform import resource_loader
